8e56d17003297107ee62be4a09915228ad5bbc06
brownleeb <brownleeb702@gmail.com>
2017-11-22 16:14:42 -0500

    Delete select.php

--- select.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (isset($_GET["opt"]) == TRUE) {
-   $LIRC_COMMAND = "sudo irsend SEND_ONCE geac ".$_GET['opt'];  }
-  else  {
-   $LIRC_COMMAND = "sudo irsend SEND_ONCE geac OFF";  }
-system($LIRC_COMMAND);
-?>
